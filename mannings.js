f06ec44716b90f4963f7a07ad51b7a2527dd0336
abu1995 <abhishekamal18@gmail.com>
2020-02-13 12:01:04 -0700

    few changes to formula

--- mannings.js
+++ mannings.js
@@ -3,6 +3,9 @@ var kn;
 var rcoeff;
 var hydrad;
 var slp;
+var width;
+var per;
+var height;
 var output;
 var display;
 
@@ -20,24 +23,39 @@ function roughnessCoeff(value) {
     rcoeff = value;
 }
 
-function hydraulicRad(value) {
-    hydrad = value;
-}
+
 
 function slopeOrgrad(value) {
     slp = value;
 }
 
+function takewidth(value) {
+    width = value;
+}
+
+function takeheight(value) {
+    height = value;
+    hydrad = (width*height)/(width+(2*height));
+}
+
+
+
+// function hydraulicRad() {
+//     hydrad = (width*height)/(width+(2*height));
+// }
+
+
+
 function finalCalcEng(){
     console.log("hi");
-    output=(crsec/1.486)*((hydrad)**(2/3))*((slp)**(1/2)); 
-    return output+" f/s" +"The feeling you get when you succesfully calculate Mannings flow velocity especially in ENGLISH units :P";
+    output=(crsec/1.486)*(hydrad)**(2/3)*((slp)**(1/2)); 
+    return output+" f/s" +"  The feeling you get when you succesfully calculate Mannings flow velocity especially in ENGLISH units :P";
     //parseFloat = document.getElementById("outputtoshow").innerHTML;
 }
 
 function finalCalcSi(){
     console.log("hi");
     output=(crsec/1.0)*((hydrad)**(2/3))*((slp)**(1/2)); 
-    return output+" m/s" + "Congrats, you are killing it !!";
+    return output+" m/s" + "  Congrats, you are killing it !!";
     //parseFloat = document.getElementById("outputtoshow").innerHTML;
 }
