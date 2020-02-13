175c9d175d7ca17672ec1c7e809689d6bb15143a
Abhishek Amalaraj <50392506+abu1995@users.noreply.github.com>
2020-02-13 10:08:11 -0700

    Update mannings.js

--- mannings.js
+++ mannings.js
@@ -31,13 +31,13 @@ function slopeOrgrad(value) {
 function finalCalcEng(){
     console.log("hi");
     output=(crsec/1.486)*((hydrad)**(2/3))*((slp)**(1/2)); 
-    return output+" f/s" +"The feeling you get when you succesfully calculate Mannings flow velocity especially in ENGLISH units :P";
+    return output+" f/s" +"  \nThe feeling you get when you succesfully calculate Mannings flow velocity especially in ENGLISH units :P";
     //parseFloat = document.getElementById("outputtoshow").innerHTML;
 }
 
 function finalCalcSi(){
     console.log("hi");
     output=(crsec/1.0)*((hydrad)**(2/3))*((slp)**(1/2)); 
-    return output+" m/s" + "Congrats, you are killing it !!";
+    return output+" m/s" + "  \nCongrats, you are killing it !!";
     //parseFloat = document.getElementById("outputtoshow").innerHTML;
 }
